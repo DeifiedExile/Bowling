190c14ce5d90a65fac8cdb986fdd53781f608bf3
Lucas Wolfs <lwolfs@my.wctc.edu>
2019-04-18 20:48:07 -0500

    Bowling Scorekeeper

---
+++ test/TestSuite.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author Lucas Wolfs <lwolfs@my.wctc.edu>
+ */
+    @RunWith(Suite.class)
+    @Suite.SuiteClasses({GameTest.class})
+public class TestSuite {
+    
+   
+    
+    
+    
+
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
